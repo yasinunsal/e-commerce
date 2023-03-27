c031b6ca32ac8faf813934e472de164f58fa3e61
yasinunsal <75205838+yasinunsal@users.noreply.github.com>
2023-03-27 21:48:38 +0300

    category add

--- src/main/java/kodlama/io/ecommerce/entities/Product.java
+++ src/main/java/kodlama/io/ecommerce/entities/Product.java
@@ -1,7 +1,10 @@
 package kodlama.io.ecommerce.entities;
 
 import jakarta.persistence.*;
+import kodlama.io.ecommerce.entities.enums.Status;
 import lombok.*;
+
+import java.util.List;
 
 @Entity
 @Getter
@@ -11,10 +14,16 @@ import lombok.*;
 @Table(name = "products")
 public class Product {
     @Id
+
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
     private int quantity;
     private double unitPrice;
     private String description;
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    @ManyToMany(mappedBy = "products")
+    private List<Category> categories;
 }
