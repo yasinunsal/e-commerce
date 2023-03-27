3a97dfea3d40e41248cff3a54d39e98ca06c6019
yasinunsal <75205838+yasinunsal@users.noreply.github.com>
2023-03-27 21:49:34 +0300

    category add

---
+++ src/main/java/kodlama/io/ecommerce/entities/Category.java
@@ -0,0 +1,27 @@
+package kodlama.io.ecommerce.entities;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+
+    @ManyToMany
+    private List<Product> products;
+
+}
